b039b7e5e6300482c9db3f63d0e269addaf92aba
Vinicius Pasquantonio <vinicius.pasquantonio@agido.com>
2022-02-25 22:03:55 +0100

    atualizando css e implementacao do app.js

--- app.js
+++ app.js
@@ -1,125 +1,145 @@
-const tileContainer = document.querySelector(".tile-container");
-const firstRowContainer = document.querySelector("#first-row-container");
-const secondRowContainer = document.querySelector("#second-row-container");
-const thirdRowContainer = document.querySelector("#third-row-container");
+const tiles = document.querySelector(".tile-container");
+const backspaceAndEnterRow = document.querySelector("#backspaceAndEnterRow");
+const keyboardFirstRow = document.querySelector("#keyboardFirstRow");
+const keyboardSecondRow = document.querySelector("#keyboardSecondRow");
+const keyboardThirdRow = document.querySelector("#keyboardThirdRow");
 
 const keysFirstRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 const keysSecondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 const keysThirdRow = ["Z", "X", "C", "V", "B", "N", "M"];
 
-const hiddenWord = "VASCO";
-const columns = 5;
 const rows = 6;
+const columns = 5;
 let currentRow = 0;
 let currentColumn = 0;
-let guesses = [];
+let letreco = "LETRA";
+let letrecoMap = {};
+for (let index = 0; index < letreco.length; index++) {
+  letrecoMap[letreco[index]] = index;
+}
+const guesses = [];
 
 for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
   guesses[rowIndex] = new Array(columns);
   const tileRow = document.createElement("div");
   tileRow.setAttribute("id", "row" + rowIndex);
-  tileRow.setAttribute("class", "row");
+  tileRow.setAttribute("class", "tile-row");
   for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-    const columnRow = document.createElement("div");
-    columnRow.setAttribute("id", "row" + rowIndex + "column" + columnIndex);
-    columnRow.setAttribute(
+    const tileColumn = document.createElement("div");
+    tileColumn.setAttribute("id", "row" + rowIndex + "column" + columnIndex);
+    tileColumn.setAttribute(
       "class",
-      rowIndex === 0 ? "column typing" : "column disabled"
+      rowIndex === 0 ? "tile-column typing" : "tile-column disabled"
     );
-    tileRow.append(columnRow);
+    tileRow.append(tileColumn);
     guesses[rowIndex][columnIndex] = "";
   }
-
-  tileContainer.append(tileRow);
+  tiles.append(tileRow);
 }
-
-keysFirstRow.forEach((key) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", () => handleKeyOnClick(key));
-  buttonElement.textContent = key;
-  firstRowContainer.append(buttonElement);
-});
-
-keysSecondRow.forEach((key) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", () => handleKeyOnClick(key));
-  buttonElement.textContent = key;
-  secondRowContainer.append(buttonElement);
-});
-
-keysThirdRow.forEach((key) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", () => handleKeyOnClick(key));
-  buttonElement.textContent = key;
-  thirdRowContainer.append(buttonElement);
-});
-document.onkeydown = function (evt) {
-  evt = evt || window.evt;
-  if (evt.key === "Enter") {
-    checkGuess();
-  } else if ((evt.key = "Backspace")) {
-    handleBackSpace();
-  } else {
-    handleKeyOnClick(evt.key.toUpperCase());
-  }
-};
-
-const handleBackSpace = () => {
-  if (currentColumn === 0) {
-    return;
-  }
-
-  currentColumn--;
-  guesses[currentRow][currentColumn] = "";
-  const tile = getCurrentColumnEl();
-  tile.textContent = "";
-};
-
-const getCurrentColumnEl = () => {
-  return document.querySelector("#row" + currentRow + "column" + currentColumn);
-};
 
 const checkGuess = () => {
   const guess = guesses[currentRow].join("");
-  if (guess === hiddenWord) {
-    window.alert("Simplesmente, o detetivão do entreterimento!!");
+  if (guess.length !== columns) {
     return;
-  } else {
-    if (currentRow === rows - 1) {
-      window.alert("Meteu essa?");
-      return;
-    } else {
-      var currentColumns = document.querySelectorAll(".typing");
-      for (let index = 0; index < currentColumns.length; index++) {
-        currentColumns[index].removeAttribute("class");
-        currentColumns[index].setAttribute("class", "column disabled");
-      }
-      currentRow++;
-      currentColumn = 0;
+  }
 
-      const currentRowEl = document.querySelector("#row" + currentRow);
-      var currentColumns = currentRowEl.querySelectorAll(".column");
-      for (let index = 0; index < currentColumns.length; index++) {
-          currentColumns[index].removeAttribute("class");
-        currentColumns[index].setAttribute("class", "column typing");
-      }
+  var currentColumns = document.querySelectorAll(".typing");
+  for (let index = 0; index < columns; index++) {
+    const letter = guess[index];
+    if (letrecoMap[letter] === undefined) {
+        currentColumns[index].classList.add("wrong")
+    } else {
+        if(letrecoMap[letter] === index) {
+            currentColumns[index].classList.add("right")
+        } else {
+            currentColumns[index].classList.add("displaced")
+        }
     }
+  }
+
+  if(guess === letreco) {
+      window.alert("tu é demais, simplesmente o detetivao do entreterimento!")
+      return
+  } {
+      if(currentRow === rows -1) {
+          window.alert("Errrrrrou!")
+      } else {
+          moveToNextRow()
+      }
   }
 };
 
-const handleKeyOnClick = (key) => {
-  if (currentRow > rows || currentRow > columns) {
-    window.alert("Game over!!!");
-    return;
-  } else if (currentColumn === columns) {
-    window.alert("Arrisca ai");
+const moveToNextRow = () => {
+    var typingColumns = document.querySelectorAll(".typing")
+    for (let index = 0; index < typingColumns.length; index++) {
+        typingColumns[index].classList.remove("typing")
+        typingColumns[index].classList.add("disabled")
+    }
+    currentRow++
+    currentColumn=0
+
+    const currentRowEl = document.querySelector("#row"+currentRow)
+    var currentColumns = currentRowEl.querySelectorAll(".tile-column")
+    for (let index = 0; index < currentColumns.length; index++) {
+        currentColumns[index].classList.remove("disabled")
+        currentColumns[index].classList.add("typing")
+    }
+}
+
+const handleKeyboardOnClick = (key) => {
+  if (currentColumn === columns) {
     return;
   }
-  const currentColumnEl = getCurrentColumnEl();
-  currentColumnEl.textContent = key;
+  const currentTile = document.querySelector(
+    "#row" + currentRow + "column" + currentColumn
+  );
+  currentTile.textContent = key;
   guesses[currentRow][currentColumn] = key;
   currentColumn++;
 };
+
+const createKeyboardRow = (keys, keyboardRow) => {
+  keys.forEach((key) => {
+    var buttonElement = document.createElement("button");
+    buttonElement.textContent = key;
+    buttonElement.setAttribute("id", key);
+    buttonElement.addEventListener("click", () => handleKeyboardOnClick(key));
+    keyboardRow.append(buttonElement);
+  });
+};
+
+createKeyboardRow(keysFirstRow, keyboardFirstRow);
+createKeyboardRow(keysSecondRow, keyboardSecondRow);
+createKeyboardRow(keysThirdRow, keyboardThirdRow);
+
+const handleBackspace = () => {
+  if(currentColumn === 0){
+      return
+  }
+
+  currentColumn--
+  guesses[currentRow][currentColumn] = ""
+  const tile = document.querySelector("#row"+currentRow+"column"+currentColumn)
+  tile.textContent = ""
+};
+
+const backspaceButton = document.createElement("button");
+backspaceButton.addEventListener("click", handleBackspace);
+backspaceButton.textContent = "<";
+backspaceAndEnterRow.append(backspaceButton);
+
+const enterButton = document.createElement("button");
+enterButton.addEventListener("click", checkGuess);
+enterButton.textContent = "ENTER";
+backspaceAndEnterRow.append(enterButton);
+
+document.onkeydown = function (evt) {
+    evt = evt || window.evt
+    if(evt.key === "Enter"){
+        checkGuess();
+    } else if (evt.key === "Backspace") {
+        handleBackspace()
+    } else {
+        handleKeyboardOnClick(evt.key.toUpperCase())
+    }
+}
